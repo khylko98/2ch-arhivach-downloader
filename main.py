a5d2501796ce79ab59f9c4051348c4db3b8c48f0
Mykyta Khylko <khylko98@gmail.com>
2024-03-13 16:56:34 +0200

    add checking processing time

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
+import time
 
 from sys import argv
-
 
 FOLDER_NAME_FROM_LINK_PATTERN = r"/(\d+)(\.html)?(#.*)?/?$"
 
@@ -26,4 +26,7 @@ def main():
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     main()
+    end_time = time.time()
+    print(f"Total processing time: {end_time - start_time} seconds")
