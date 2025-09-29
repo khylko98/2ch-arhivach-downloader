8cce5bf8aeddd5f863015a1032912c3ab3e621b0
Mykyta Khylko <khylko98@gmail.com>
2025-09-29 13:06:29 +0300

    clean useless commands and add comments

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-base_python_interpreter=""
-project_path="pwd"
+set -e
 
-read -p "Python interpreter: " base_python_interpreter
+read -p "Python interpreter (e.g. python3.11): " base_python_interpreter
 
-`$base_python_interpreter -m venv venv`
+# Создание виртуального окружения
+$base_python_interpreter -m venv venv
 
+# Активация окружения
 source ./venv/bin/activate
 
+# Установка зависимостей
 pip install -U pip
 pip install -r requirements.txt
